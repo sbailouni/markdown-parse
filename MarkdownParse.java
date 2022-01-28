61116f61acee495b1a6dea4c003b46f40e083a10
sbailouni <97708686+sbailouni@users.noreply.github.com>
2022-01-27 23:20:15 -0800

    Fixed link in first line error

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -22,7 +22,7 @@ public class MarkdownParse {
                 continue;
             }
             int closeParen = markdown.indexOf(")", openParen);
-            if(markdown.charAt(nextOpenBracket-1) == '!') {
+            if(nextOpenBracket != 0 && markdown.charAt(nextOpenBracket-1) == '!') {
                 currentIndex = closeParen + 1;
                 continue;
             }
